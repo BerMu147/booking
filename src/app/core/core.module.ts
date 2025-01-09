c159924d6091cc07c3e6a8955a61bfb36e602002
BerMu147 <berin.mujcinovic@edu.fit.ba>
2025-01-09 13:56:31 +0100

    Registered interceptions in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,18 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: []
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ]
 })
 
 export class CoreModule{
