de6dcbb9158fda76f9bef0a1443bda4710c25311
BerMu147 <berin.mujcinovic@edu.fit.ba>
2025-01-14 15:11:53 +0100

    Added registration to auth module

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/core.module';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../../shared/components/login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '', redirectTo:'login', pathMatch:'full'}
+  {path: '', redirectTo:'login', pathMatch:'full'},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     SharedModule,
     RouterModule.forChild(routes)
